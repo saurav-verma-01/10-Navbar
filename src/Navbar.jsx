0dd420a5ce6291d114a1e784d0fc5662664f3049
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-03-06 17:02:05 +0530

    project structure complete

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,35 +1,56 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import logo from "./logo.svg";
 import { FaBars } from "react-icons/fa";
-import { links } from "./data";
+import { links, social } from "./data";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleShow = () => {
+    // const linksHeight = linksRef.current.getBoundingClientRect();
+    // console.log(linksHeight.height);
+    setShowLinks((prev) => !prev);
+  };
+
+  const linksStyle = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
   return (
     <header className="header">
       <nav>
         <img src={logo} alt="nav logo" className="nav-logo" />
 
-        <button
-          onClick={() => setShowLinks((prev) => !prev)}
-          className="nav-toggle"
-        >
+        <button onClick={toggleShow} className="nav-toggle">
           <FaBars />
         </button>
       </nav>
 
-      <div className="links-container">
-        <ul className={"links"}>
+      <div
+        style={linksStyle}
+        ref={linksContainerRef}
+        className="links-container"
+      >
+        <ul ref={linksRef} className={"links"}>
           {links.map((link) => (
-            <li
-              className={showLinks ? "show-links" : "hide-links"}
-              key={link.id}
-            >
+            <li key={link.id}>
               <a href={link.url}>{link.text}</a>
             </li>
           ))}
         </ul>
       </div>
+
+      <ul className="socials">
+        {social.map((item) => (
+          <li key={item.id}>
+            <a href={item.url}>{item.icon}</a>
+          </li>
+        ))}
+      </ul>
     </header>
   );
 };
